937717ce3d805fbdd0ceb576736a7dc11d3d226a
Rimero Solutions <infocom@rimerosolutions.com>
2013-07-13 15:37:50 -0400

    Update javadoc and license header.

--- src/main/java/com/rimerosolutions/ant/git/tasks/GitSettingsTask.java
+++ src/main/java/com/rimerosolutions/ant/git/tasks/GitSettingsTask.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013, Rimero Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.rimerosolutions.ant.git.tasks;
 
 import org.apache.tools.ant.BuildException;
@@ -8,6 +23,11 @@ import org.apache.tools.ant.types.Reference;
 import com.rimerosolutions.ant.git.GitSettings;
 import com.rimerosolutions.ant.git.GitUtils;
 
+/**
+ * Sets reusable Git settings (credentials and identity)
+ *
+ * @author Yves Zoundi
+ */
 public class GitSettingsTask extends Task {
 
         private String name;
@@ -16,37 +36,46 @@ public class GitSettingsTask extends Task {
         private String password;
         private String refId;
 
+        /**
+         * Sets a project reference id for reuse
+         *
+         * @param refId The reference id to set
+         */
         public void setRefId(String refId) {
                 this.refId = refId;
         }
 
         /**
-         * @param name
-         *                the name to set
+         * Sets the user's name
+         *
+         * @param name the name to set
          */
         public void setName(String name) {
                 this.name = name;
         }
 
         /**
-         * @param email
-         *                the email to set
+         * Sets the user's email
+         *
+         * @param email the email to set
          */
         public void setEmail(String email) {
                 this.email = email;
         }
 
         /**
-         * @param username
-         *                the username to set
+         * Sets the user's name
+         *
+         * @param username the username to set
          */
         public void setUsername(String username) {
                 this.username = username;
         }
 
         /**
-         * @param password
-         *                the password to set
+         * Sets the user's password
+         *
+         * @param password the password to set
          */
         public void setPassword(String password) {
                 this.password = password;
